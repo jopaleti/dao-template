f5e3d128d66af5828e1e501398fd9eaf075e004d
jopaleti <oopaletijohnson@gmail.com>
2024-09-27 23:23:40 +0100

    ...

--- deploy/01-deploy-governor-token.ts
+++ deploy/01-deploy-governor-token.ts
@@ -36,23 +36,3 @@ const delegate = async (governanceTokenAddress: string, delegatedAccount: string
 
 export default deployGovernanceToken
 deployGovernanceToken.tags = ["all", "governor"]
-
-
-
-// import { HardhatRuntimeEnvironment } from "hardhat/types";
-// import { DeployFunction } from "hardhat-deploy/types";
-
-// const deployGovernanceToken: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-//     const { getNamedAccounts, deployments, network } = hre;
-//     const { deploy, log } = deployments;
-//     const { deployer } = await getNamedAccounts();
-//     log("Deploying Governance Token...")
-//     const governanceToken = await deploy("GovernanceToken", {
-//         from: deployer,
-//         args: [],
-//         log: true
-//     })
-//     log(`Deployed governance token to address ${governanceToken.address}`)
-// }
-
-// export default deployGovernanceToken;
---
+++ deploy/03-deploy-governor-contract.ts
@@ -0,0 +1,3 @@
+import { HardforkHistoryConfig } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import verify from "../helper-functions";
