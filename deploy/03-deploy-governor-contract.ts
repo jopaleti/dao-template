98d0ecad9e15f1f1e5aa0aff8e13811890140fc0
jopaleti <oopaletijohnson@gmail.com>
2024-09-27 23:35:40 +0100

    ...

--- deploy/03-deploy-governor-contract.ts
+++ deploy/03-deploy-governor-contract.ts
@@ -1,3 +1,21 @@
-import { HardforkHistoryConfig } from "hardhat/types";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import verify from "../helper-functions";
+import {
+  networkConfig,
+  developmentChains,
+  QUORUM_PERCENTAGE,
+  VOTING_PERIOD,
+  VOTING_DELAY,
+} from "../helper-hardhat-config";
+
+const deployGovernorContract: DeployFunction = async function (hre:HardhatRuntimeEnvironment) {
+    const { getNamedAccounts, deployments, network } = hre;
+    const { deploy, log, get } = deployments;
+    const { deployer } = await getNamedAccounts();
+    const governanceToken = await get("GovernanceToken")
+    const timeLock = await get("TimeLock");
+    const args = [
+        
+    ]
+}
